112c3f40eb0ebf81ef0557656d6f9735aa652190
wortz <jp_pp_hi5@hotmail.com>
2020-07-23 18:34:48 +0100

    Updates

---
+++ precache-manifest.2c8c0aec300a4330339623f4f30d7369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5b7e68982b6bae9ce60cd9971d4e18",
+    "url": "/SashHelper/index.html"
+  },
+  {
+    "revision": "1b9f47af67e1c87e18cc",
+    "url": "/SashHelper/static/css/main.b6604ee0.chunk.css"
+  },
+  {
+    "revision": "cd33f1592bc60134fe1c",
+    "url": "/SashHelper/static/js/2.6a0ae403.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SashHelper/static/js/2.6a0ae403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b9f47af67e1c87e18cc",
+    "url": "/SashHelper/static/js/main.692bc197.chunk.js"
+  },
+  {
+    "revision": "88cac71b85388ea05628",
+    "url": "/SashHelper/static/js/runtime-main.a53c91a4.js"
+  }
+]);
