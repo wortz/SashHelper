05daef232abbfc1e161a824a7008fb725cf0b29b
wortz <jp_pp_hi5@hotmail.com>
2020-07-25 20:32:23 +0100

    Feature Bonus cannot be repeted now

--- src/Components/Elements/Bonus.js
+++ src/Components/Elements/Bonus.js
@@ -2,22 +2,41 @@ import React from 'react';
 import bonuses from '../../StaticValues/WeaponBonuses'
 
 function RenderBonus(props){
-  const bonusesOption = []
-  bonuses.forEach((element,i) => {
-    if(i>1 || props.avg)
+
+  function checkRepeted(element){
+    for (let index = 0; index < props.bonus.length; index++) {
+      const item = props.bonus[index];
+      if(item.name === element){
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const bonusesAvailable = bonuses.slice(props.avg ? 0 : 2);
+  const bonusesOption = [];
+  for (let i = 0; i < bonusesAvailable.length; i++) {
+    const element = bonusesAvailable[i];
+    if(checkRepeted(element)){
+      bonusesAvailable.splice(i,1);
+      i--;
+    }
+    else
       bonusesOption.push(<option key={element} value={element}>{element}</option>)
-  });
+  };
+
   return (
     <div className="Bonuses">
       {props.bonus.map((item,i) => (
       <div className="SingleBonus" key={i}>
         <select name="bonuses" onChange={e => props.onChange(e,i,0)}>
+          <option key={item.name} value={item.name}>{item.name}</option>
           {bonusesOption}
         </select>
         <input type="text" placeholder="Valor do Bonus" value={item.bonus} onChange={e => props.onChange(e,i,1)} />
       </div>
     ))}
-      <button onClick={() => props.addBonus(props.avg ? bonuses[0] : bonuses[2])}>
+      <button onClick={() => props.addBonus(bonusesAvailable[0])}>
         New Bonus
       </button>
     </div>
