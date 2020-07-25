96077ae71193be17776b45d4df53e6c68bf66413
wortz <jp_pp_hi5@hotmail.com>
2020-07-25 18:48:13 +0100

    Fix error in attack speed values of phoenix bow

--- src/StaticValues/WeaponsValues.js
+++ src/StaticValues/WeaponsValues.js
@@ -25,25 +25,25 @@ const WeaponsValues = [
         values: [
             [
                 {name:"Attack Value", bonus:[311,510]},
-                {name:"Attack Speed", bonus:24},
+                {name:"Attack Speed", bonus:15},
                 {name:"Half Humans", bonus:8},
                 {name:"Strength against monsters", bonus:4},
             ],
             [
                 {name:"Attack Value", bonus:[348,547]},
-                {name:"Attack Speed", bonus:25},
+                {name:"Attack Speed", bonus:15},
                 {name:"Half Humans", bonus:10},
                 {name:"Strength against monsters", bonus:4},
             ],
             [
                 {name:"Attack Value", bonus:[404,603]},
-                {name:"Attack Speed", bonus:26},
+                {name:"Attack Speed", bonus:15},
                 {name:"Half Humans", bonus:12},
                 {name:"Strength against monsters", bonus:5},
             ],
             [
                 {name:"Attack Value", bonus:[487,686]},
-                {name:"Attack Speed", bonus:27},
+                {name:"Attack Speed", bonus:15},
                 {name:"Half Humans", bonus:15},
                 {name:"Strength against monsters", bonus:5},
             ],
